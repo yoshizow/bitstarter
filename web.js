49c8fae1161dc89babe3e57e115a68d751b7dbb8
Yoshitaro Makise <yoshizow@turtlewalk.org>
2013-07-07 16:45:38 +0900

    Read from index.html.

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  //response.send('Hello World 2!');
+  var buf = fs.readFileSync('index.html');
+  var str = buf.toString();
+  response.send(str);
 });
 
 var port = process.env.PORT || 5000;
